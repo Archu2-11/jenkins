3ac6514bc76d55e83d1a5cd65c8f31baa973b0dc
Archana k g <157709994+Archu2-11@users.noreply.github.com>
2025-03-10 10:18:41 +0530

    Create prime.java

---
+++ prime.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        // Create a scanner object to read input
+        Scanner scanner = new Scanner(System.in);
+
+        // Ask the user to input a number
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        // Check if the number is prime
+        boolean isPrime = true;
+
+        if (number <= 1) {
+            isPrime = false;  // Numbers less than or equal to 1 are not prime
+        } else {
+            // Check for factors other than 1 and the number itself
+            for (int i = 2; i <= Math.sqrt(number); i++) {
+                if (number % i == 0) {
+                    isPrime = false;  // If divisible, it's not prime
+                    break;
+                }
+            }
+        }
+
+        // Output the result
+        if (isPrime) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+
+        // Close the scanner
+        scanner.close();
+    }
+}
